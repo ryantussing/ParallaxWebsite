71f8db9f304a65dce57aa97197229868748f06b5
ryantussing <56258028+ryantussing@users.noreply.github.com>
2020-11-12 13:39:21 -0500

    2nd edits
    Functional

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-blah blah
+window.addEventListener("scroll", function() {
+    const distance = window.scrollY;
+    this.document.querySelector("header").style.transform = `translateY(${distance *
+    1} px)`;
+    this.document.querySelector(
+        ".MountainImage"
+    ).style.transform = `translateY(${distance * 0.3}px)`;
+    this.setTimeout(() => {
+        this.document.querySelector("section").classList.add("animate-me");
+    }, 400);
+});
